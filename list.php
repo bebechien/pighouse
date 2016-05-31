176a956397e11102d913b29dede5f4b32d7298f8
bebechien <bebechien@gmail.com>
2016-05-31 16:33:35 +0900

    Update list.php
    정렬 옵션 추가

--- list.php
+++ list.php
@@ -4,30 +4,40 @@ $txt = file_get_contents('http://remote.url/list.txt');
 echo $txt;
 ?>
 
-// video 폴더 밑에 있는 mp4 목록을 만드는 예제 (최신 파일 순으로 정렬)
+// video 폴더 밑에 있는 mp4 목록을 만드는 예제 (정렬 옵션 추가)
 <?php
-function scan_dir($dir) {
-    $ignored = array('.', '..', '.svn', '.htaccess');
+function scan_dir($dir, $sort_type) {
+  $ignored = array('.', '..', '.svn', '.htaccess');
 
-    $files = array();    
-    foreach (scandir($dir) as $file) {
-        if (in_array($file, $ignored)) continue;
-        $files[$file] = filemtime($dir . '/' . $file);
-    }
+  $files = array();    
+  foreach (scandir($dir) as $file) {
+    if (in_array($file, $ignored)) continue;
+    $files[$file] = filemtime($dir . '/' . $file);
+  }
 
-    arsort($files);
+  if (empty($sort_type)) {
     $files = array_keys($files);
+  }
+  else {
+    if($sort_type == "oldest") {
+      asort($files);
+    }
+    else if($sort_type == "latest") {
+      arsort($files);
+    }
+    $files = array_keys($files);
+  }
 
-    return ($files) ? $files : false;
+  return ($files) ? $files : false;
 }
 
-function file_list($d,$x) {
-  foreach(scan_dir($d) as $f)if(is_file($d.'/'.$f)&&(($x)?ereg($x.'$',$f):1))$l[]=$f;
+function file_list($d,$x, $sort_type) {
+  foreach(scan_dir($d, $sort_type) as $f)if(is_file($d.'/'.$f)&&(($x)?ereg($x.'$',$f):1))$l[]=$f;
   return $l;
 }
 
-function listup($d, $prefix) {
-  $list = file_list($d, "mp4");
+function listup($d, $prefix, $sort_type) {
+  $list = file_list($d, "mp4", $sort_type);
   foreach($list as $file) :
     if (!empty($prefix)) {
       print($prefix."http://server.url/".$d.rawurlencode($file)."\n");
@@ -38,5 +48,12 @@ function listup($d, $prefix) {
   endforeach;
 }
 
-listup("video/", "");
+// sort by name
+listup("video/", "", "oldest");
+
+// oldest to latest
+listup("video/", "", "oldest");
+// latest to oldest
+listup("video/", "", "latest");
+
 ?>
