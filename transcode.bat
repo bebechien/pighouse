bd6e4926b334e815b6ec9a7681d1c69fe0d05bb3
bebechien <bebechien@gmail.com>
2016-11-04 15:17:20 +0900

    Create transcode.bat
    입력받은 폴더 안의 파일을 모두 변환하는 batch

---
+++ transcode.bat
@@ -0,0 +1,9 @@
+@echo off
+set argC=0
+for %%x in (%*) do Set /A argC+=1
+
+IF %argC% == 2 (
+    for /R %2 %%f in (*) do HandBrakeCLI-10bit.exe -i "%%f" -o "%%~nf.%1" -e x265_10bit --encoder-profile main10 -E copy
+) ELSE (
+    echo Usage: %0 format srcdir
+)
