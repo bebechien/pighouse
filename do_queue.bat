973f175391732ca5dcd93bc97f9051e4493d97ca
bebechien <bebechien@gmail.com>
2016-11-04 15:16:14 +0900

    Create do_queue.bat
    queue에 파일이 다 없어질때까지 transcoding 하는 batch file

---
+++ do_queue.bat
@@ -0,0 +1,23 @@
+@echo off
+set argC=0
+for %%x in (%*) do Set /A argC+=1
+
+IF %argC% == 1 (
+    mkdir %1
+
+    :COPYFILES
+    if not exist queue\* (goto NOFILES)
+
+    FOR %%f IN (queue\*) DO (
+        move "%%f" %1
+        HandBrakeCLI-10bit.exe --vfr -b 1024k -i "%~1\%%~nxf" -o "%%~nf.mp4" -e x265_10bit --encoder-profile main10 -E copy
+        goto COPYFILES
+    )
+
+    :NOFILES
+    set /p DUMMY=Job done! Hit ENTER to continue...
+    goto :EOF
+
+) ELSE (
+    echo Usage: %0 working_dir
+)
