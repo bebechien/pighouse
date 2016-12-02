4d2223c4cc28c90e48ca2cc12957aaff02aa7712
bebechien <bebechien@gmail.com>
2016-12-02 11:12:47 +0900

    Update transcode.bat
    모든 오디오, 자막 복사

--- transcode.bat
+++ transcode.bat
@@ -3,7 +3,7 @@ set argC=0
 for %%x in (%*) do Set /A argC+=1
 
 IF %argC% == 2 (
-    for /R %2 %%f in (*) do HandBrakeCLI-10bit.exe -i "%%f" -o "%%~nf.%1" -e x265_10bit --encoder-profile main10 -E copy
+    for /R %2 %%f in (*) do HandBrakeCLI-10bit.exe -i "%%f" -o "%%~nf.%1" -e x265_10bit --encoder-profile main10 -E copy --all-audio --all-subtitles
 ) ELSE (
     echo Usage: %0 format srcdir
 )
