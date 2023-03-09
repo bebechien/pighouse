4b20eef931386ab0912903fcbfefd6d1be376b5c
Juyeong Ji <bebechien@gmail.com>
2023-03-09 11:06:25 +0900

    Update zip.bat
    path에 상관없이 동작하도록 수정

--- zip.bat
+++ zip.bat
@@ -1,17 +1,20 @@
 mkdir done
 mkdir trash
+mkdir working
+
+SET curpath=%~dp0
 
 cd queue
 for /D %%r in (*.*) do (
   cd "%%r"
   for /D %%d in (*.*) do (
-    "C:\Program Files\7-Zip\7z.exe" a -tzip "C:\Users\bebechien\Desktop\mm\%%d.zip" "%%d"
+    "C:\Program Files\7-Zip\7z.exe" a -tzip "%curpath%working\%%d.zip" "%%d"
   )
   cd ..
 
 :: from queue
   mkdir "..\done\%%r"
-  mv -v "C:\Users\bebechien\Desktop\mm"/* "..\done\%%r"
+  mv -v "%curpath%working"/* "..\done\%%r"
   mv "%%r" ..\trash
 )
 
